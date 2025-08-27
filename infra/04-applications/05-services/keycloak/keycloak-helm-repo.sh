d9e085c5dc07395e6e7c36770d608fc04824a93b
Cyphle <cyril.phamle@gmail.com>
2025-08-27 17:54:50 +0200

    keycloak

--- infra/04-applications/05-services/keycloak/keycloak-helm-repo.sh
+++ infra/04-applications/05-services/keycloak/keycloak-helm-repo.sh
@@ -6,3 +6,4 @@ helm repo update
 
 helm install keycloak bitnami/keycloak -f keycloak-values.yaml --namespace banana
 # helm uninstall -n banana keycloak
+# helm upgrade keycloak bitnami/keycloak -n banana -f keycloak-values.yaml --atomic --wait --timeout 10m
